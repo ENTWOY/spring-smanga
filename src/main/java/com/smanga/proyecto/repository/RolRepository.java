21db46792ff23816ea2beb05592ffe3f98000d7c
Neiser <Neiser@gmail.com>
2023-06-25 19:59:43 -0500

    seccion noticias agregado correctamente

--- src/main/java/com/smanga/proyecto/repository/RolRepository.java
+++ src/main/java/com/smanga/proyecto/repository/RolRepository.java
@@ -1,5 +1,7 @@
 package com.smanga.proyecto.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.smanga.proyecto.entity.Rol;
 @Repository
 public interface RolRepository extends JpaRepository<Rol, Integer>{
 
+	//Método para buscar un role por su nombre en nuestra base de datos
+    Optional<Rol> findByNomRol(String name);
 }
